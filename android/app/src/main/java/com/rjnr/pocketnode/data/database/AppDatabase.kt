aad58eb8d0d9c6cae0493339e638a7bd7d71d51b
raheemjnr <mumedian6@gmail.com>
2026-02-25 01:32:32 +0100

    feat: add HeaderCacheEntity and DaoCellEntity to AppDatabase v2 (Issue #40 Phase 2)

--- android/app/src/main/java/com/rjnr/pocketnode/data/database/AppDatabase.kt
+++ android/app/src/main/java/com/rjnr/pocketnode/data/database/AppDatabase.kt
@@ -3,16 +3,27 @@ package com.rjnr.pocketnode.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.rjnr.pocketnode.data.database.dao.BalanceCacheDao
+import com.rjnr.pocketnode.data.database.dao.DaoCellDao
+import com.rjnr.pocketnode.data.database.dao.HeaderCacheDao
 import com.rjnr.pocketnode.data.database.dao.TransactionDao
 import com.rjnr.pocketnode.data.database.entity.BalanceCacheEntity
+import com.rjnr.pocketnode.data.database.entity.DaoCellEntity
+import com.rjnr.pocketnode.data.database.entity.HeaderCacheEntity
 import com.rjnr.pocketnode.data.database.entity.TransactionEntity
 
 @Database(
-    entities = [TransactionEntity::class, BalanceCacheEntity::class],
-    version = 1,
+    entities = [
+        TransactionEntity::class,
+        BalanceCacheEntity::class,
+        HeaderCacheEntity::class,
+        DaoCellEntity::class
+    ],
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
     abstract fun balanceCacheDao(): BalanceCacheDao
+    abstract fun headerCacheDao(): HeaderCacheDao
+    abstract fun daoCellDao(): DaoCellDao
 }
